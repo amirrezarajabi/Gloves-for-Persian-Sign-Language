24db02dcf00d9e9c72db33e9d3d57bad2aecb5bf
amirrezarajabi <amir.reza.rajabi13800201@gmail.com>
2023-03-29 18:23:34 -0400

    2nd try for train

--- model.py
+++ model.py
@@ -4,10 +4,10 @@ from conf import CLASSES
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.rnn1 = nn.LSTM(10, 32, 1, batch_first=True)
-        self.rnn2 = nn.LSTM(32, 128, 1, batch_first=True)
-        self.linear1 = nn.Linear(128, 64)
-        self.linear2 = nn.Linear(64, len(CLASSES) + 1)
+        self.rnn1 = nn.LSTM(10, 16, 1, batch_first=True)
+        self.rnn2 = nn.LSTM(16, 64, 1, batch_first=True)
+        self.linear1 = nn.Linear(64, 32)
+        self.linear2 = nn.Linear(32, len(CLASSES) + 1)
     def forward(self, x):
         out, _ = self.rnn1(x)
         out = nn.Tanh()(out)
